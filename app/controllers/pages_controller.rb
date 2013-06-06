8b8f18736b78f640ddf75426a755bea40143b617
Johnny Chen <jcc0821@gmail.com>
2013-06-06 16:56:41 -0400

    home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @a = Wordnik.word.get_definitions('hysterical')
+    @b = 'hello'
+
+    @adjective1 = Wordnik.words.randomWord(includePartOfSpeech: "adjective", minLength: "1")
+    @adjective2 = Wordnik.words.randomWord(hasDictionaryDef: "false", includePartOfSpeech: "adjective", minLength: "1")
+
+    @adverb1 = Wordnik.words.randomWord(includePartOfSpeech: "adverb", minLength: "1")
+    @adverb2 = Wordnik.words.randomWord(hasDictionaryDef: "false", includePartOfSpeech: "adverb", minLength: "1")
   end
 
   def result
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Nicknames::Application.routes.draw do
 
-  get "pages/home"
+root to: 'pages#home'
 
-  get "pages/result"
-
-  root to: "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
